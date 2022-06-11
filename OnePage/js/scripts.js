92a7429b88b00bda5541558dc0d3ee8c180a6ef0
elrafa1981 <rafaelcarrasco88@gmail.com>
2022-06-11 19:05:41 -0300

    Se agrego un comentario en la linea 29

--- OnePage/js/scripts.js
+++ OnePage/js/scripts.js
@@ -26,6 +26,8 @@ window.addEventListener('DOMContentLoaded', event => {
     // Shrink the navbar 
     navbarShrink();
 
+    //Aca esta hecho el cambio
+
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
